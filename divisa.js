9358a63f402a66459e17f1206e692ded828456e5
patriciob <patobarragan3@gmail.com>
2022-09-08 00:31:11 -0300

    refactor(divisa.js): improved logic now there is only one function

--- divisa.js
+++ divisa.js
@@ -1,28 +1,14 @@
-var valuePeso = 1;
-var valueDolar = 286;
-var valueEuro = 305;
+coinObj  = {
+    usdToArs: 286,
+    eurToArs: 305,
+    yuanToArs: 20,
+    bolToArs: 10,
+}
 
-const currencyEuroDolar = (valueDolar, valueEuro, valuePeso, amount) => {
-    return  (valuePeso/valueDolar) / (valuePeso/valueEuro) * (amount)
+const conversor = (coinZeroToArs, currencyOneToArs, amountCoinZero) => {
+   var amountCoinOne = (coinZeroToArs/currencyOneToArs) * amountCoinZero;
+   return amountCoinOne;
 }
 
 
-const currencyDolarEuro = (valueDolar, valueEuro, valuePeso, amount) => {
-    return  (valuePeso/valueEuro) / (valuePeso/valueDolar) * (amount)
-}
-
-
-const currencyPesoDolar= (valueDolar, valueEuro, valuePeso, amount) => {
-    return    valueDolar * (amount)
-}
-
-
-const currencyPesoEuro= (valueDolar, valueEuro, valuePeso, amount) => {
-    return    valueEuro * (amount)
-}
-
-
-console.log(currencyPesoEuro(valueDolar, valueEuro, valuePeso, 2));//Peso a Euro
-console.log(currencyPesoDolar(valueDolar, valueEuro, valuePeso, 2));//Peso a Dolar
-console.log(currencyDolarEuro(valueDolar, valueEuro, valuePeso, 2));//Dolar a Euro
-console.log(currencyEuroDolar(valueDolar, valueEuro, valuePeso, 2));//Euro a Dolar
+console.log(conversor(coinObj.yuanToArs, coinObj.bolToArs, 3)); 
