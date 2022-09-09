ed197d0828bd49c6e90e6ae443d61ebe6ca75b00
patriciob <patobarragan3@gmail.com>
2022-09-09 20:17:40 -0300

    refactor(scripts/divisas.js): change logic, add assertion

--- scripts/divisa.js
+++ scripts/divisa.js
@@ -2,26 +2,28 @@
 // en este archivo deberia quedar solo la funcion
 function conversor(coinZero, coinOne, amountCoinZero) {
   if (
-    typeof coinZero === "string" &&
-    typeof coinOne === "string" &&
-    typeof amountCoinZero === "number"
+    typeof coinZero !== "string" ||
+    typeof coinOne !== "string" ||
+    typeof amountCoinZero !== "number"
   ) {
-    const coinObj = {
-      usdToArs: 286,
-      eurToArs: 305,
-      yuanToArs: 20,
-      bolToArs: 10,
-    };
-
-    const coinZeroToArs = coinObj[coinZero];
-    const currencyOneToArs = coinObj[coinOne];
-
-    var amountCoinOne = (coinZeroToArs / currencyOneToArs) * amountCoinZero;
-    return amountCoinOne;
-  } else {
-    throw new Error("only numbers in amount");
+    throw new Error("Params are bad");
   }
-}
+  const coinObj = {
+    usdToArs: 286,
+    eurToArs: 305,
+    yuanToArs: 20,
+    bolToArs: 10,
+  };
 
+  const coinZeroToArs = coinObj[coinZero];
+  const currencyOneToArs = coinObj[coinOne];
+
+  if (!coinZeroToArs || !currencyOneToArs) {
+    throw new Error("Currency does not exist");
+  }
+
+  var amountCoinOne = (coinZeroToArs / currencyOneToArs) * amountCoinZero;
+  return amountCoinOne;
+}
 // que entre por el string del nombre de la moneda y vos busques el precio
 console.log(conversor("bolToArs", "yuanToArs", 3));
